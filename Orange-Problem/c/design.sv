fabd781682349f27b9fa2a0194a56855f8d19f1e
prxnav2005 <pranavmofficial2005@gmail.com>
2024-11-24 22:32:57 +0530

    c part done

---
+++ Orange-Problem/c/design.sv
@@ -0,0 +1,4 @@
+module func(input logic a, b, c, d, output logic y, z);
+    assign y = d | (a & ~b & c);
+    assign z = (b & d) | (a & ~c & d);
+endmodule
---
+++ Orange-Problem/c/tb.sv
@@ -0,0 +1,25 @@
+module func_tb;
+    reg a, b, c, d;
+    wire y, z;
+
+    func dut(a, b, c, d, y, z);
+
+    initial begin
+        a = 1'b0; b = 1'b0; c = 1'b0; d = 1'b0; #10;
+        a = 1'b0; b = 1'b0; c = 1'b0; d = 1'b1; #10;
+        a = 1'b0; b = 1'b0; c = 1'b1; d = 1'b0; #10;
+        a = 1'b0; b = 1'b0; c = 1'b1; d = 1'b1; #10;
+        a = 1'b0; b = 1'b1; c = 1'b0; d = 1'b0; #10;
+        a = 1'b0; b = 1'b1; c = 1'b0; d = 1'b1; #10;
+        a = 1'b0; b = 1'b1; c = 1'b1; d = 1'b0; #10;
+        a = 1'b0; b = 1'b1; c = 1'b1; d = 1'b1; #10;
+        a = 1'b1; b = 1'b0; c = 1'b0; d = 1'b0; #10;
+        a = 1'b1; b = 1'b0; c = 1'b0; d = 1'b1; #10;
+        a = 1'b1; b = 1'b0; c = 1'b1; d = 1'b0; #10;
+        a = 1'b1; b = 1'b0; c = 1'b1; d = 1'b1; #10;
+        a = 1'b1; b = 1'b1; c = 1'b0; d = 1'b0; #10;
+        a = 1'b1; b = 1'b1; c = 1'b0; d = 1'b1; #10;
+        a = 1'b1; b = 1'b1; c = 1'b1; d = 1'b0; #10;
+        a = 1'b1; b = 1'b1; c = 1'b1; d = 1'b1; #10;
+    end
+endmodule

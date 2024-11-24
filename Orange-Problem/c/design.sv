4d09c0b5e6deb587f2dda1c33f50bc1436802bae
prxnav2005 <pranavmofficial2005@gmail.com>
2024-11-24 22:37:38 +0530

    d part done

--- Orange-Problem/c/design.sv
+++ Orange-Problem/c/design.sv
@@ -1,4 +1,4 @@
-module func(input logic a, b, c, d, output logic y, z);
+module func3(input logic a, b, c, d, output logic y, z);
     assign y = d | (a & ~b & c);
     assign z = (b & d) | (a & ~c & d);
 endmodule
